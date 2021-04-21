73f69ca831cece67be7e91dca71fdd22948cc226
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2021-04-21 16:47:15 -0500

    07 - Validaciones

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -13,12 +13,18 @@
 
             <div class="mb-4">
                 <x-jet-label value="Título del post"/>
-                <x-jet-input type="text" class="w-full" wire:model.defer="title"/>
+                <x-jet-input type="text" class="w-full" wire:model="title"/>
+
+                <x-jet-input-error for="title"/>
+
             </div>
 
             <div class="mb-4">
                 <x-jet-label value="Contenido del post"/>
-                <textarea wire:model.defer="content" class="form-control w-full" rows="6"></textarea>
+                <textarea wire:model="content" class="form-control w-full" rows="6"></textarea>
+
+                <x-jet-input-error for="content"/>
+
             </div>
 
         </x-slot>
