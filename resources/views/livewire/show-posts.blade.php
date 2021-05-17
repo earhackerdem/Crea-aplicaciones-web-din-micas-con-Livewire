4f19b4dd5b1e8fe425a05b87caf16a30160088fa
Earvin Saúl Pérez Ramos <earhackerdem@gmailcom>
2021-05-17 17:47:46 -0500

    11 - Pasar parámetros de acción

--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -10,7 +10,8 @@
 
             <div class="px-6 py-4 flex items-center">
                 {{-- <input type="text" wire:model="search"> --}}
-                <x-jet-input class="flex-1 mr-4" placeholder="Escriba que quiere buscar" type="text" wire:model="search" />
+                <x-jet-input class="flex-1 mr-4" placeholder="Escriba que quiere buscar" type="text"
+                    wire:model="search" />
                 @livewire('create-post')
             </div>
             @if ($posts->count())
@@ -73,27 +74,31 @@
                     </thead>
                     <tbody class="bg-white divide-y divide-gray-200">
 
-                        @foreach ($posts as $post)
+                        @foreach ($posts as $item)
 
                             <tr>
 
                                 <td class="px-6 py-4 ">
                                     <div class="text-sm text-gray-900">
-                                        {{ $post->id }}
+                                        {{ $item->id }}
                                     </div>
                                 </td>
                                 <td class="px-6 py-4 ">
                                     <div class="text-sm text-gray-900">
-                                        {{ $post->title }}
+                                        {{ $item->title }}
                                     </div>
                                 </td>
                                 <td class="px-6 py-4  text-sm text-gray-500">
                                     <div class="text-sm text-gray-900">
-                                        {{ $post->content }}
+                                        {{ $item->content }}
                                     </div>
                                 </td>
                                 <td class="px-6 py-4  ext-sm font-medium">
-                                   @livewire('edit-post',['post'=>$post],key($post->id))
+                                    {{-- @livewire('edit-item',['item'=>$item],key($item->id)) --}}
+                                    <a class="btn btn-green" wire:click="edit({{ $item }})">
+                                        <i class="fas fa-edit"></i>
+                                    </a>
+
                                 </td>
                             </tr>
                         @endforeach
@@ -109,5 +114,55 @@
         </x-table>
 
     </div>
+
+    <x-jet-dialog-modal wire:model="open_edit">
+
+        <x-slot name="title">
+            Editar el post
+        </x-slot>
+
+        <x-slot name="content">
+
+            <div wire:loading wire:target="image"
+                class="mb-4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                <strong class="font-bold">¡Imagen cargado!</strong>
+                <span class="block sm:inline">Espere un momento hasta que la imagen se haya procesado</span>
+
+            </div>
+
+            @if ($image)
+                <img class="mb-4" src="{{ $image->temporaryUrl() }}" alt="">
+            @else
+                <img src="{{Storage::url($post->image)}}" alt="">
+            @endif
+
+            <div class="mb-4">
+                <x-jet-label value="Título del post"/>
+                <x-jet-input wire:model="post.title" type="text" class="w-full"/>
+            </div>
+
+            <div>
+                <x-jet-label value="Contenido del post"/>
+                <textarea wire:model="post.content" rows="6" class="form-control w-full"></textarea>
+            </div>
+
+            <div>
+                <input type="file" wire:model="image" id="{{ $identificador }}">
+                <x-jet-input-error for="image"/>
+            </div>
+
+        </x-slot>
+
+        <x-slot name="footer">
+            <x-jet-secondary-button wire:click="$set('open_edit',false)">
+                Cancelar
+            </x-jet-secondary-button>
+
+            <x-jet-danger-button wire:click="update" wire:loading.attr="disabled" class="disabled:opacity-25">
+                Actualizar
+            </x-jet-danger-button>
+        </x-slot>
+
+    </x-jet-dialog-modal>
 
 </div>
